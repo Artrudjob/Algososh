e428a81acbf8746ee48e8a5f796e3369e657b110
Artrudjob <Artrudjob@yandex.ru>
2022-10-11 12:47:57 +0300

    feat: create linkedListNode

---
+++ src/components/list-page/linkedListNode.ts
@@ -0,0 +1,14 @@
+export interface INode<T> {
+    value: T;
+    next: LinkedListNode<T> | null;
+}
+
+export default class LinkedListNode<T> implements INode<T>{
+    constructor(
+        public value: T,
+        public next: LinkedListNode<T> | null
+    ) {
+        this.value = value;
+        this.next = next;
+    }
+}
